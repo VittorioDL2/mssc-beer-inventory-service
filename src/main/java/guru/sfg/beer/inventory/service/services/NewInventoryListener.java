d42fbdffe388e5a211b1cf3e8adc3db8cb0a066b
REPLYNET\v.dileo <v.dileo@reply.it>
2022-07-12 14:37:12 +0200

    JSM for beer inventory service

--- src/main/java/guru/sfg/beer/inventory/service/services/NewInventoryListener.java
+++ src/main/java/guru/sfg/beer/inventory/service/services/NewInventoryListener.java
@@ -1,7 +1,10 @@
 package guru.sfg.beer.inventory.service.services;
 
+import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
+import guru.sfg.beer.inventory.service.config.JmsConfig;
+import guru.sfg.beer.inventory.service.domain.BeerInventory;
 import guru.sfg.beer.inventory.service.repositories.BeerInventoryRepository;
 import guru.sfg.common.events.NewInventoryEvent;
 import lombok.RequiredArgsConstructor;
@@ -13,7 +16,13 @@ import lombok.extern.slf4j.Slf4j;
 public class NewInventoryListener {
 	private final BeerInventoryRepository beerInventoryRepository;
 	
+	@JmsListener(destination = JmsConfig.NEW_INVENTORY_QUEUE)
 	public void listen(NewInventoryEvent newInventoryEvent) {
+		log.debug("Got inventory : " + newInventoryEvent.toString());
 		
+		beerInventoryRepository.save(BeerInventory.builder().beerId(newInventoryEvent.getBeerDto().getId())
+				.upc(newInventoryEvent.getBeerDto().getUpc())
+				.quantityOnHand(newInventoryEvent.getBeerDto().getQuantityOnHand())
+				.build());
 	}
 }
